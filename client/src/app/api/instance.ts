783afd5bea3a4bb0c60c6d87804847e3dcc26572
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2024-01-24 15:10:34 +0545

    didnt worked

--- client/src/app/api/instance.ts
+++ client/src/app/api/instance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 //https://online-cv-builder.onrender.com
 
 export const instance = axios.create({
-    baseURL: "https://online-cv-builder-s6fx.vercel.app/api/users",
+    baseURL: "https://online-cv-builder.onrender.com/api/users",
     withCredentials: true,
     
   });
