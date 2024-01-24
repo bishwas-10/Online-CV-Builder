5fe03dcf0417d76f8a47f5ee824644714602554d
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2024-01-24 14:56:08 +0545

    some minor changes lets see

--- client/src/app/api/instance.ts
+++ client/src/app/api/instance.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 //https://online-cv-builder.onrender.com
 
 export const instance = axios.create({
-    baseURL: "https://online-cv-builder.onrender.com/api/users",
+    baseURL: "https://online-cv-builder-s6fx.vercel.app/api/users",
     withCredentials: true,
     
   });
-
+//https://online-cv-builder-s6fx.vercel.app/
  // http://localhost:4000
