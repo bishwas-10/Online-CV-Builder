7a1a27ca2348cdbee1d245efc5b4975952cecb9d
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2024-07-23 08:13:01 +0545

    initial commit for v0.2

--- server/utils/createRefreshToken.ts
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-import jwt from "jsonwebtoken";
-
-const createRefreshToken= (id : string)=>{
-    return jwt.sign({id}, process.env.REFRESH_TOKEN_KEY as string,{ expiresIn: '2h' } )
-}
-export default createRefreshToken;
--- server/utils/secretToken.ts
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-import jwt from "jsonwebtoken";
-
-const createSecretToken= (id : string)=>{
-    return jwt.sign({id}, process.env.TOKEN_KEY as string,{expiresIn:'5m'});
-}
-export default createSecretToken;
